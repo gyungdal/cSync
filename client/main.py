c9575624f857a97ea7f206f4daae822f53c45b93
Gyeongsik Kim <gyungdal@thes.kr>
2019-04-24 18:00:39 +0900

    client 소켓 정리

--- client/main.py
+++ client/main.py
@@ -1,30 +1,22 @@
 # -*- coding: utf8 -*-
-from socket import *
+import socket
 import json
 import time
-import picamera
-
-
-def capture():
-    camera = picamera.PiCamera()
-    camera.resolution = (3280, 2464)
-    camera.start_preview()
-    camera.capture('test.png')
-    camera.close()
-
 
 def waitServer():
-    s = socket(AF_INET, SOCK_DGRAM)
-    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('', 8000))
-    while True:
-        msg, _ = s.recvfrom(1024)  # 브로드케스트 서버의 전송을 기다린다.
-        recv = json.loads(msg.decode())
-        if recv["service"] == "cSync":
-            return recv
-
+    msg, _ = s.recvfrom(1024)  # 브로드케스트 서버의 전송을 기다린다.
+    recv = json.loads(msg.decode())
+    s.close()
+    if recv["service"] == "cSync":
+        return recv
+    else:
+        return None
 
 if __name__ == "__main__":
-    config = waitServer()
+    config = None
+    while config == None :
+        config = waitServer()
     print(config)
-    capture()
