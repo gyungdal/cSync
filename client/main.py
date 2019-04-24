b1d73eb782cdae24eeeb07ed7e12d74b58c17939
Gyeongsik Kim <gyungdal@thes.kr>
2019-04-24 17:04:51 +0900

    ntp server 추가 reindent

--- client/main.py
+++ client/main.py
@@ -4,22 +4,25 @@ import json
 import time
 import picamera
 
-def capture():   
-    camera = picamera.PiCamera() 
+
+def capture():
+    camera = picamera.PiCamera()
     camera.resolution = (3280, 2464)
     camera.start_preview()
     camera.capture('test.png')
     camera.close()
 
+
 def waitServer():
     s = socket(AF_INET, SOCK_DGRAM)
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    s.bind(('',8000))
+    s.bind(('', 8000))
     while True:
-        msg, addr = s.recvfrom(1024) # 브로드케스트 서버의 전송을 기다린다.
+        msg, addr = s.recvfrom(1024)  # 브로드케스트 서버의 전송을 기다린다.
         recv = json.loads(msg.decode())
-        if recv["service"] == "cSync" : 
+        if recv["service"] == "cSync":
             return recv
+
 
 if __name__ == "__main__":
     config = waitServer()
--- server/main.py
+++ server/main.py
@@ -2,13 +2,14 @@ import socket
 import time
 import json
 
-broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+broadcast = socket.socket(
+    socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 broadcast.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 broadcast.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 broadcast.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 serverConfig = json.dumps({
-    "service" : "cSync",
-    "ip" : socket.gethostbyname(socket.gethostname()),
-    "port" : 3000
+    "service": "cSync",
+    "ip": socket.gethostbyname(socket.gethostname()),
+    "port": 3000
 })
 broadcast.sendto(bytearray(serverConfig.encode()), ("192.168.0.255", 8000))
