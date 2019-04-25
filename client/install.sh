79fb0fa7b61222ccda99a743fe8a218dd2336cad
Gyeongsik Kim <gyungdal@thes.kr>
2019-04-25 16:44:37 +0900

    edit install script

--- client/install.sh
+++ client/install.sh
@@ -4,4 +4,9 @@ sudo apt-get install vim git openssh-server python3 python3-dev python3-pip -y
 pip3 install picamera
 pip3 install ntplib
 
+sudo echo `network={ \
+    ssid="testing" \
+    psk="testingPassword" \
+}` >> /etc/wpa_supplicant/wpa_supplicant.conf
+
 sudo reboot
