2b83da595fd96bd6c4b1c25d7aa6f5c0ea3a25db
Gyeongsik Kim <gyungdal@thes.kr>
2019-04-25 17:45:20 +0900

    script 폴더 분리 및 파이썬 설치 스크립트 추가

--- script/install.sh
+++ script/install.sh
@@ -1,11 +1,5 @@
 sudo apt-get update
 sudo apt-get install vim git openssh-server python3 python3-dev python3-pip -y
-sudo apt-get install python3.7 -y
-
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
-
-sudo update-alternatives --config python3
 # type 2
 
 pip3 install picamera
