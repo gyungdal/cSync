ed1ffbab28832efe5432eaae5b54d02509ccda64
Gyeongsik Kim <gyungdal@thes.kr>
2019-04-25 17:11:33 +0900

    install script error fix

--- client/install.sh
+++ client/install.sh
@@ -4,9 +4,9 @@ sudo apt-get install vim git openssh-server python3 python3-dev python3-pip -y
 pip3 install picamera
 pip3 install ntplib
 
-sudo echo `network={ \
-    ssid="testing" \
-    psk="testingPassword" \
-}` >> /etc/wpa_supplicant/wpa_supplicant.conf
+sudo echo 'network={ \
+    ssid="iptime" \
+    key_mgmt=NONE \
+}' >> /etc/wpa_supplicant/wpa_supplicant.conf
 
 sudo reboot
