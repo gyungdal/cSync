2b9c5d03459bba5f6d8628d305dddd243936b766
Gyeongsik Kim <gyungdal@thes.kr>
2019-04-25 10:06:46 +0900

    fileserver 테스트용 코드 추가
    fileserver stand alone 코드 추가

---
+++ test/file.py
@@ -0,0 +1,14 @@
+import socket
+import sys
+
+ip = '127.0.0.1'
+port = int(sys.argv[1])
+size = 1024
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((ip, port))
+s.send(bytearray("Hello".encode()))
+data = s.recv(size)
+ 
+print(data.decode())
+s.close()
