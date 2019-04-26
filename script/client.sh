dc79169451d97d945ce88ca18dcdd31996517e09
Gyeongsik Kim <gyungdal@thes.kr>
2019-04-26 09:13:10 +0900

    스크립트 간소화 클라이언트 시작 스크립트추가

---
+++ script/client.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/python3.7 /home/pi/cSync/client/main.py
--- script/install.sh
+++ script/install.sh
@@ -1,9 +1,20 @@
 sudo apt-get update
-sudo apt-get install vim git openssh-server python3 python3-dev python3-pip -y
-# type 2
+sudo apt-get install vim git openssh-server python3 python3-dev python3-pip build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+tar xf Python-3.7.0.tar.xz
+cd Python-3.7.0
+./configure
+make -j 4
+sudo make altinstall
+cd ..
+sudo rm -r Python-3.7.0
+rm Python-3.7.0.tar.xz
+sudo apt-get --purge remove build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+sudo apt-get autoremove -y
+sudo apt-get clean
 
-pip3 install picamera
-pip3 install ntplib
+pip3.7 install picamera --user
+pip3.7 install ntplib --user
 
 sudo echo 'network={ \
     ssid="iptime" \
