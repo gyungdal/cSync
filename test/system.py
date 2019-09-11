2f0fd82a1fd5dc26c7f267e21aa0a495523d93bb
Gyeongsik Kim <gyungdal@the-s.kr>
2019-09-11 11:35:13 +0900

    Update system.py
    pytest는 함수명 앞에 test가 들어가야 인식한다고함

--- test/system.py
+++ test/system.py
@@ -1,5 +1,8 @@
 import sys
 import pytest
 
-def python_version_check():
-    assert "3.7" in sys.version
+def test_python_version():
+    if "3.7" in sys.version:
+        pass
+    else:
+        pytest.fail("Python Version : {}".format(sys.version))
