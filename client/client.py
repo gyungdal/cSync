b9423a330299c44631bc80ae5917bc1fa738b4eb
Gyeongsik Kim <gyungdal@the-s.kr>
2019-09-11 11:05:51 +0900

    오토포커스 코드 추가

--- client/client.py
+++ client/client.py
@@ -11,7 +11,15 @@ import io
 import socket
 import picamera
 import RPi.GPIO as GPIO
+import cv2
+import numpy as py
 
+try:
+	import picamera
+	from picamera.array import PiRGBArray
+except:
+	sys.exit(0)
+	
 class Client(Communcation):
     def __init__(self, config = {}, debug=False):
         sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,6 +36,30 @@ class Client(Communcation):
         GPIO.setup(5, GPIO.OUT)
         GPIO.setup(6, GPIO.OUT)
         self.setGPIO(True)
+
+    def focusing(self, val):
+        value = (val << 4) & 0x3ff0
+        data1 = (value >> 8) & 0x3f
+        data2 = value & 0xf0
+        os.system("i2cset -y 0 0x0c %d %d" % (data1,data2))
+        
+    def sobel(self, img):
+        img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+        img_sobel = cv2.Sobel(img_gray,cv2.CV_16U,1,1)
+        return cv2.mean(img_sobel)[0]
+
+    def laplacian(self, img):
+        img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+        img_sobel = cv2.Laplacian(img_gray,cv2.CV_16U)
+        return cv2.mean(img_sobel)[0]
+        
+
+    def calculation(self, camera):
+        rawCapture = PiRGBArray(camera) 
+        camera.capture(rawCapture,format="bgr", use_video_port=True)
+        image = rawCapture.array
+        rawCapture.truncate(0)
+        return laplacian(image)
 
     def setGPIO(self, status : bool):
         GPIO.output(5, status)
@@ -54,6 +86,58 @@ class Client(Communcation):
         data = StatusData()
         response = self.ntp.request(self.config['ip'], port=self.config['port']['ntp'])
         data.diff = response.delay
+        camera = picamera.PiCamera()
+        #open camera preview
+        camera.start_preview()
+        #set camera resolution to 640x480(Small resolution for faster speeds.)
+        camera.resolution = (640, 480)
+
+        print("Start focusing")
+        
+        max_index = 10
+        max_value = 0.0
+        last_value = 0.0
+        dec_count = 0
+        focal_distance = 10
+        
+        while True:
+            #Adjust focus
+            focusing(focal_distance)
+            #Take image and calculate image clarity
+            val = calculation(camera)
+            #Find the maximum image clarity
+            if val > max_value:
+                max_index = focal_distance
+                max_value = val
+                
+            #If the image clarity starts to decrease
+            if val < last_value:
+                dec_count += 1
+            else:
+                dec_count = 0
+            #Image clarity is reduced by six consecutive frames
+            if dec_count > 6:
+                break
+            last_value = val
+            
+            #Increase the focal distance
+            focal_distance += 10
+            if focal_distance > 1000:
+                break
+
+        #Adjust focus to the best
+        focusing(max_index)
+        time.sleep(1)
+        #set camera resolution to 2592x1944
+        camera.resolution = (2592,1944)
+        #save image to file.
+        camera.capture("test.jpg")
+        print("max index = %d,max value = %lf" % (max_index,max_value))
+        #while True:
+        #	time.sleep(1)
+            
+        camera.stop_preview()
+        camera.close()
         data.status = CameraStatus.OK
         packet = Packet(PacketType.RESPONSE_STATUS, data)
         self.sendPickle(packet.toPickle())
