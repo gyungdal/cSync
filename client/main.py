049891306865095952dc155ecf3a2695b0306715
Gyeongsik Kim <gyungdal@the-s.kr>
2019-11-28 17:26:51 +0900

    edit client code for support v2 packet

--- client/main.py
+++ client/main.py
@@ -2,52 +2,56 @@
 import socket
 import pickle
 import time
+import logging
+import subprocess
+import signal
+import sys
 
-from client import Client
+class Client():
+    def __init__(self):
+        self.processList = []
+        self.handler = {
+            "update" : self.update,
+            "update-zip" : self.update,
+            "start" : self.start,
+            "stop" : self.kill
+        }
 
-# https://picamera.readthedocs.io/en/release-1.13/recipes1.html
-
-DEBUG = True
-comm = None
-
-def waitServer():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('', 8000))
-        msg, _ = s.recvfrom(1024)  # 브로드케스트 서버의 전송을 기다린다.
-        recv = pickle.loads(msg)
-        s.close()
-        if recv["service"] == "cSync":
-            return recv
-        else:
-            return None
-    except Exception as e:
-        if DEBUG:
-            print("[ERROR] : Server broadcast data crash")
-            print("\tㄴ Description -> " + e)
-        return None
+    def update(self, packet):
+        pass
+    def start(self, packet):
+        pass
     
+    def kill(self, packet):
+        for process in self.processList :
+            process.kill()
 
-def startThread():
-    global comm
-    comm = Client(config, debug=DEBUG)
-    comm.start()
-    
+    def waitServer(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            s.bind(('', 8080))
+            msg, _ = s.recvfrom(1024)  # 브로드케스트 서버의 전송을 기다린다.
+            recv = pickle.loads(msg)
+            s.close()
+            if recv["service"] == "cSync":
+                if recv["command"] in self.handler:
+                    self.handler[recv["command"]](recv)
+
+    def dispose(self):
+        self.kill()
+        
+client = Client()
+
+def signalHandler(signum, frame):
+    global client
+    client.dispose()
+    sys.exit()
+
 if __name__ == "__main__":
-    try: 
-        while True:
-            config = waitServer()
-            if DEBUG:
-                print(config)
-            if comm != None:
-                if not comm.isAlive():
-                    startThread()
-            else:
-                startThread()
-    except Exception as ex: # 에러 종류
-        if DEBUG:
-            print('[ERROR] : ', ex)
-    finally:
-        if comm != None:
-            comm.stop()
+    signal.signal(signal.SIGINT, signalHandler)
+    signal.signal(signal.SIGTERM, signalHandler)
+    signal.signal(signal.SIGKILL, signalHandler)
+    client = Client()
+    while True:
+        client.waitServer()
