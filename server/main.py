c8faca656439c1f8b6892f9b06aaeb3200af5c6e
Gyeongsik Kim <gyungdal@the-s.kr>
2020-04-06 16:59:18 +0900

    브로드캐스트 테스트 추가

--- server/main.py
+++ server/main.py
@@ -1,36 +1,47 @@
-from utils import hereAmI
-from file_server import fileServer
-from time import sleep
+import signal 
+import logging
+from sys import exit
 
-fileInstance = fileServer()
-FLAG = True
+logger = logging.getLogger(__name__)
+def signalHandler(signum, frame):
+    logger.critical("Exit Signal %d".format(signum))
+    exit()
 
-def stop():
-    global FLAG
-    FLAG = False            
-    
-def broadcast():
-    hereAmI(fileServerPort=fileInstance.getPort(), ntpPort=ntpInstance.getPort())
+def get_ip_address() -> str :
+    from sys import platform
+    import netifaces as ni
+    ifname = "eth0"
+    if platform.startswith('darwin'):
+        ifname = 'en0'
+    ni.ifaddresses(ifname)
+    return ni.ifaddresses(ifname)[ni.AF_INET][0]['addr']
+
+def find_device(message : str, broadcast_ip : str):
+    import socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    logger.debug('send message : %s' % message)
+    server.sendto(message.encode(),  (broadcast_ip, 8001))
+    server.close()
+
+async def main(broadcast_ip : str):
+    from json import dumps
+    message = {}
+    message['test'] = "value"
+    data = dumps(message)
+    print(data)
+    print(type(data))
+    find_device(data, broadcast_ip)
 
 if __name__ == "__main__":
-    fileInstance.start()
-    ntpInstance.start()
-    
-    try:
-        while FLAG:
-            value = input("CAPTURE?\nc : Capture\ns : status\nb : Broadcast\ne : stop\n")
-            HANDLER_TABLE = {
-                'c' : fileInstance.capture,
-                'b' : broadcast,
-                's' : fileInstance.getStatus,
-                'e' : stop
-            }
-            if value in HANDLER_TABLE.keys():
-                HANDLER_TABLE[value]()
-    except KeyboardInterrupt:
-        FLAG = False
-    finally:
-        ntpInstance.stop()
-        fileInstance.stop()
-        print("Exited")
-        
+    from asyncio import run
+    signal.signal(signal.SIGINT, signalHandler)
+    signal.signal(signal.SIGTERM, signalHandler)
+    #signal.signal(signal.SIGKILL, signalHandler)
+    local_ip = get_ip_address().split('.')
+    local_ip[3] = '255'
+    broadcast_ip : str = '.'.join(local_ip)
+    logger.info("broadcast ip : %s" % broadcast_ip)
+    print(broadcast_ip)
+    run(main(broadcast_ip))
